164e0c294aac50c109b822043915b5a8c10203ab
andreas <andreas@immerda.ch>
2007-12-11 10:26:14 +0000

    aide modul

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+# modules/aide/manifests/init.pp - manage aide
+# Copyright (C) 2007 admin@immerda.ch
+# 
+
+modules_dir { "aide": }
+
+class aide {
+
+	package { 'aide':
+                ensure => present,
+                category => $operatingsystem ? {
+                        gentoo => 'app-forensics',
+                        default => '',
+                },
+        }
+
+
+	#service { aide: ensure  => running, enable  => true, }
+
+#	file {
+#        	"/etc/aide/aide.conf":
+#        		ensure => file,
+#        		force => true,
+#        		mode => 0644, owner => root, group => root;
+#        }
+	
+}
+
