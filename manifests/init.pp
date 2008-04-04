411d957f94b32c3171ef0ae446cae4407a7cb11c
mh <mh@immerda.ch>
2008-04-04 15:30:26 +0000

    moved /dist/-stuff to /files/, heavily refactored a lot, made it more look all the same way. some sources added here and there. hope it still works everything, the location on the disk of /files/ is still call dists. no need to hurry to change that

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,41 +3,50 @@
 # 
 
 class aide {
+    case $operatingsystem {
+        gentoo: { include aide::gentoo }
+        default: { include aide::base }
+    }
 
-	package { 'aide':
-                ensure => present,
-                category => $operatingsystem ? {
-                        gentoo => 'app-forensics',
-                        default => '',
-                },
-        }
-
-
-	#service { aide: ensure  => running, enable  => true, }
-
-	file {
-        	"/etc/aide/aide.conf":
-                source => "puppet://$servername/aide/aide.conf",
-        		ensure => file,
-        		force => true,
-        		mode => 0644, owner => root, group => 0;
-        }
-	file {
-        	"/etc/cron.daily/aide.cron":
-                source => "puppet://$servername/aide/aide.cron",
-        	    ensure => file,
-        	    force => true,
-        	    mode => 0755, owner => root, group => 0;
-        }
-	file {
-        	"/var/lib/aide/aide.db":
-                source => [
-                    "puppet://$servername/dist/aide/${fqdn}/aide.db",
-                    "puppet://$servername/dist/aide/immer1-0.glei.ch/aide.db"
-                ],
-        	    ensure => file,
-        	    force => true,
-                mode => 0400, owner => root, group => 0;
-        }
 }
 
+class aide::base {
+	package { 'aide':
+        ensure => present,
+    }
+
+	file { "/etc/aide/aide.conf":
+        source => [ "puppet://$servername/files/aide/${fqdn}/aide.conf",
+                    "puppet://$servername/files/aide/aide.conf",
+                    "puppet://$servername/aide/aide.conf"
+                ],
+        ensure => file,
+        force => true,
+        mode => 0644, owner => root, group => 0;
+    }
+	file { "/etc/cron.daily/aide.cron":
+        source => [ "puppet://$servername/files/aide/${fqdn}/aide.cron",
+                    "puppet://$servername/files/aide/aide.cron",
+                    "puppet://$servername/aide/aide.cron"
+                ],
+        ensure => file,
+        force => true,
+        mode => 0755, owner => root, group => 0;
+    }
+	file { "/var/lib/aide/aide.db":
+        source => [
+            "puppet://$servername/files/aide/${fqdn}/aide.db",
+            "puppet://$servername/files/aide/aide.db",
+            "puppet://$servername/aide/aide.db"
+        ],
+        ensure => file,
+        force => true,
+        mode => 0400, owner => root, group => 0;
+    }
+}
+
+class aide::gentoo inherits aide::base {
+    Package[aide] {
+        category => 'app-forensics',
+    }
+}
