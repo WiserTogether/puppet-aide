4c05adb9d2a2c5347840a63589044cb8afa65039
mh <mh@immerda.ch>
2010-08-07 02:12:11 +0200

    remove unnecessary fileserver variable (#2460)

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,29 +4,29 @@ class aide::base {
     }
 
 	file { "/etc/aide.conf":
-        source => [ "puppet://$server/modules/site-aide/${fqdn}/aide.conf",
-                    "puppet://$server/modules/site-aide/${operatingsystem}/aide.conf",
-                    "puppet://$server/modules/site-aide/aide.conf",
-                    "puppet://$server/modules/aide/${operatingsystem}/aide.conf",
-                    "puppet://$server/modules/aide/aide.conf"
+        source => [ "puppet:///modules/site-aide/${fqdn}/aide.conf",
+                    "puppet:///modules/site-aide/${operatingsystem}/aide.conf",
+                    "puppet:///modules/site-aide/aide.conf",
+                    "puppet:///modules/aide/${operatingsystem}/aide.conf",
+                    "puppet:///modules/aide/aide.conf"
                 ],
         ensure => file,
         force => true,
         mode => 0644, owner => root, group => 0;
     }
 	file { "/etc/cron.daily/aide.cron":
-        source => [ "puppet://$server/modules/site-aide/${fqdn}/aide.cron",
-                    "puppet://$server/modules/site-aide/${operatingsystem}/aide.cron",
-                    "puppet://$server/modules/site-aide/aide.cron",
-                    "puppet://$server/modules/aide/${operatingsystem}/aide.cron",
-                    "puppet://$server/modules/aide/aide.cron"
+        source => [ "puppet:///modules/site-aide/${fqdn}/aide.cron",
+                    "puppet:///modules/site-aide/${operatingsystem}/aide.cron",
+                    "puppet:///modules/site-aide/aide.cron",
+                    "puppet:///modules/aide/${operatingsystem}/aide.cron",
+                    "puppet:///modules/aide/aide.cron"
                 ],
         ensure => file,
         force => true,
         mode => 0755, owner => root, group => 0;
     }
 	file { "/var/lib/aide/aide.db":
-        source => "puppet://$server/modules/site-aide/${fqdn}/aide.db",
+        source => "puppet:///modules/site-aide/${fqdn}/aide.db",
         ensure => file,
         force => true,
         mode => 0400, owner => root, group => 0;
